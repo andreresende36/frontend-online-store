fe59aebfd3f7585e8da5bdc6a519eda9e4cf9f55
Guilherme Marques <guimarquesjob@gmail.com>
2023-01-11 17:55:08 -0300

    grupo-20-a-req3-finished

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import React from 'react';
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Link } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Cart from './pages/Cart';
 
 function App() {
   return (
-
-    <div>
-      <Switch>
-        <Route exact path="/" component={ Header } />
-      </Switch>
-
-    </div>
+    <>
+      <div>
+        <Switch>
+          <Route exact path="/" component={ Header } />
+          <Route exact path="/cart" component={ Cart } />
+        </Switch>
+      </div>
+    </>
   );
 }
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class Header extends Component {
   render() {
@@ -15,6 +16,9 @@ export default class Header extends Component {
         <p data-testid="home-initial-message">
           Digite algum termo de pesquisa ou escolha uma categoria.
         </p>
+        <Link data-testid="shopping-cart-button" to="/cart">
+          <button type="button">Carrinho de Compras</button>
+        </Link>
 
       </header>
     );
